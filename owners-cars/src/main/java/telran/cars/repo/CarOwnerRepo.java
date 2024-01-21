223020e543476b4843c7f8721a50cce6fa5ec93b
Daniel Kleyman <denny_klm1@yahoo.com>
2024-01-21 15:48:53 +0200

    HW-64

--- owners-cars/src/main/java/telran/cars/repo/CarOwnerRepo.java
+++ owners-cars/src/main/java/telran/cars/repo/CarOwnerRepo.java
@@ -1,9 +1,12 @@
 package telran.cars.repo;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import telran.cars.dto.EnginePowerCapacity;
 import telran.cars.service.model.*;
 
 public interface CarOwnerRepo extends JpaRepository<CarOwner, Long> {
-
-}
+	
+} 
