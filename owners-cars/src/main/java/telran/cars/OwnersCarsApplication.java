369813d11b41ec10c3496ca430f15b97705ea695
Daniel Kleyman <denny_klm1@yahoo.com>
2024-01-23 15:18:43 +0200

    HW-64-with console for jpql queries testing

--- owners-cars/src/main/java/telran/cars/OwnersCarsApplication.java
+++ owners-cars/src/main/java/telran/cars/OwnersCarsApplication.java
@@ -1,13 +1,22 @@
 package telran.cars;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OwnersCarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OwnersCarsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runQueries(JPQLQueryConsole console) {
+		return args -> {
+			console.run(); // Run the console interactively
+		};
 	}
 
 }
