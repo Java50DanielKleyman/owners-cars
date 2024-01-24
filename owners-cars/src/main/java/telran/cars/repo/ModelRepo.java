c8136e5a7a73518331a7a29cb32af4e5e49cf785
Daniel Kleyman <denny_klm1@yahoo.com>
2024-01-24 15:44:30 +0200

    HW-65

--- owners-cars/src/main/java/telran/cars/repo/ModelRepo.java
+++ owners-cars/src/main/java/telran/cars/repo/ModelRepo.java
@@ -10,12 +10,22 @@ import telran.cars.dto.ModelNameAmount;
 import telran.cars.service.model.*;
 
 public interface ModelRepo extends JpaRepository<Model, ModelYear> {
-@Query(value="select model_name from cars join trade_deals td"
-		+ " on cars.car_number=td.car_number group by model_name "
-		+ " having count(*) = (select max(count) from "
-		+ "(select count(*) as count from cars join trade_deals "
-		+ " on cars.car_number = trade_deals.car_number "
-		+ "group by model_name)) ", nativeQuery=true) //just SQL query
+//@Query(value="select model_name from cars join trade_deals td"
+//		+ " on cars.car_number=td.car_number group by model_name "
+//		+ " having count(*) = (select max(count) from "
+//		+ "(select count(*) as count from cars join trade_deals "
+//		+ " on cars.car_number = trade_deals.car_number "
+//		+ "group by model_name)) ", nativeQuery=true) //just SQL query
+@Query("select car.model.modelYear.name "
+		+ "from Car car "
+		+ "join TradeDeal tradeDeal on car.number = tradeDeal.car.number "
+		+ "group by car.model.modelYear.name "
+		+ "having count(*) = (select max(countModels) "
+		+ "from "
+		+ "(select count(*) as countModels "
+		+ "from Car car1 "
+		+ "join TradeDeal tradeDeal1  on car1.number = tradeDeal1.car.number "
+		+ "group by car1.model.modelYear.name))")
 	List<String> findMostSoldModelNames();
 /*************************************************************/
 @Query(value="select c.model_name as name, count(*) as amount "
